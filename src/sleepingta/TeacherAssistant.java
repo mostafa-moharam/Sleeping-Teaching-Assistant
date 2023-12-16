3a62047f465106d495077b9771790ab3708c5a60
mostafa-moharam <mostafamoharam26@gmail.com>
2023-12-16 22:00:08 +0200

    All Source code

---
+++ src/sleepingta/TeacherAssistant.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sleepingta;
+
+/**
+ *
+ * @author mahmo
+ */
+public class TeacherAssistant implements Runnable{
+    
+    private Hall hall;
+    private int TA_ID;
+
+    public TeacherAssistant(Hall hall, int TA_ID) {
+        this.hall = hall;
+        this.TA_ID = TA_ID;
+    }
+    
+    @Override
+    public void run(){
+        while (true) {            
+            hall.AnswerQuestion(TA_ID);
+        }
+    }
+}
